96dc865963aeb7b56b6339affd3622db5293b0fc
Gino Zaccardelli <ginozacc@gmail.com>
2020-09-02 21:07:49 -0400

    After many tribulations, the final commit

--- server.js
+++ server.js
@@ -2,25 +2,44 @@ var express = require("express");
 var path = require("path");
 var dbFile = require("./db/db.json");
 var fs = require("fs");
+const { v4: uuidv4 } = require('uuid');
 
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+
+const writetoDb = res => {
+    fs.writeFile("./db/db.json", JSON.stringify(dbFile), err => {
+        if (err) throw err;
+        res.sendStatus(200);
+    });
+};
 
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
 app.get("/api/notes", function (req, res) {
-    return res.json(dbFile);
+    res.json(dbFile);
 });
 
-app.post("api/notes"), function (req, res) {
-    
-};
+app.post("/api/notes", function (req, res) {
+    const note = req.body;
+    note.id = uuidv4();
+    dbFile.push(note);
+    console.log(dbFile);
+    writetoDb(res);
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+    const id = req.params.id;
+    dbFile = dbFile.filter(note => note.id !== id);
+    console.log(dbFile);
+    writetoDb(res);
+});
 
 app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/index.html"));
